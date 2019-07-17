31013207d5d320b39f0260c49ab3b4e919595e08
planza02 <52959265+planza02@users.noreply.github.com>
2019-07-17 13:21:11 +0200

    Add files via upload

--- Controllers/deviceController.js
+++ Controllers/deviceController.js
@@ -16,9 +16,26 @@ module.exports.createDevice = function(req, res) {
 
   newDevice.save().then(function(device) {
     if (device) {
-      res.status(200).send("Funciona");
+      res.status(200).send("Device Created");
     } else {
-      res.status(400).send("No Funciona");
+      res.status(400).send("Device Creation Failed");
+    }
+  });
+};
+
+module.exports.getDevices = function(req, res) {
+  deviceModel.find({}).then(function(devices) {
+    res.json(devices);
+  });
+};
+
+module.exports.getDeviceByID = function(req, res) {
+  const deviceID = req.query.id;
+  deviceModel.findById(deviceID).then(function(device) {
+    if (device) {
+      res.json(device);
+    } else {
+      res.status(404).send("No device found with this ID");
     }
   });
 };
--- Routes/routes.js
+++ Routes/routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const deviceController = require("../Controllers/deviceController");
 
 router.post("/create-device", deviceController.createDevice);
+router.get("/devices", deviceController.getDevices);
+router.get("/device", deviceController.getDeviceByID);
 
 module.exports = router;
